41f53718b5a54018ef4d086f054109c1790f288d
RenatoXSR <renatoxsr@gmail.com>
2016-03-25 23:55:05 -0300

    passport config preparation

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ var path = require('path');
 var express = require('express');
 var config = require('./config')();
 
+//=======================================================
+// Setup for passport auth
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ========================================
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+// app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'esteéumprogramaQUeestáUsandpeoSEcret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+//==============================
+// Resume normal app routines
 var app = express();
 
 app.disable('etag');
@@ -12,6 +45,7 @@ app.set('view engine', 'jade');
 app.set('trust proxy', true);
 //app.configure('development', function () { app.locals.pretty = true; });
 
+
 // Processos
 var processosModelo = require('./processos/processosModelo')(config);
 app.use('/processos', require('./processos/crud')(processosModelo));
@@ -19,10 +53,10 @@ app.use('/api/processos', require('./processos/api')(processosModelo));
 //var classesProcessuais = require('./processos/classesProcessuais');
 //console.log(classesProcessuais);
 
-// Redirect root to /books
-app.get('/', function (req, res) {
-  res.redirect('/processos');
-});
+// routes ================================================
+require('./routes.js')(app, passport);
+// load our routes and pass in our app and fully configured passport
+
 
 // Basic 404 handler
 app.use(function (req, res) {
