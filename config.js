24554f0aff0df2b7276142347d2f6187c3c2f786
RenatoXSR <renatoxsr@gmail.com>
2016-05-03 11:50:15 -0300

    final config of env variables.

--- config.js
+++ config.js
@@ -6,10 +6,12 @@ var getConfig = module.exports = function () {
   const mongodbName = process.env.MONGODB_NAME || 'test';
   const mongodbDomain = (process.env.MONGODB_HOST || process.env.OPENSHIFT_MONGODB_DB_HOST) || '127.0.0.1';
   const mongodbPort = (process.env.MONGODB_PORT || process.env.OPENSHIFT_MONGODB_DB_PORT) || '27017';
-  const mongodbUri = 'mongodb://'+mongodbUser+':'+mongodbPassword+'@'+mongodbHost+':'++'/'+mongodbName;
+  const mongodbUri = process.env.MONGOLAB_URI || 'mongodb://'+mongodbUser+':'+mongodbPassword+'@'+mongodbHost+':'++'/'+mongodbName;
+  const serverIp = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  const serverPort = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT;
 
   return {
-    port: process.env.PORT || 8080,
+    port: serverPort || 8080,
     // dataBackend can be 'datastore', 'cloudsql', or 'mongodb'.
     dataBackend: process.env.BACKEND || 'mongodb',
 
