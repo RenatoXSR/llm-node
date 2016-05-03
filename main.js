07c50ee7e7e4ba4a72ebe45ae494e0f5b923a1ee
RenatoXSR <renatoxsr@gmail.com>
2016-05-03 13:47:08 -0300

    chose body-parser default options

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ var app = express();
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({extended: false})); // get information from html forms
 
 // app.set('view engine', 'ejs'); // set up ejs for templating
 
