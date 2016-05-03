c263f55fcc42ae91a269bdea8aeb4d206cccfbae
RenatoXSR <renatoxsr@gmail.com>
2016-05-02 21:49:53 -0300

    db name

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ const serverIp = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const dbHost = process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost';
 const dbPort = process.env.OPENSHIFT_MONGODB_DB_PORT || '27017';
-const dbUri = process.env.MONGOLAB_URI || 'mongodb://'+dbHost+':'+dbPort+'/test';
+const dbName = process.env.OPENSHIFT_
+const dbUri = process.env.MONGOLAB_URI || 'mongodb://'+dbHost+':'+dbPort+'/llm-node';
 
 
 
